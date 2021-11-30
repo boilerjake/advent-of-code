ffc826cfc2224a9ed8f30093b054c2b01a142dc5
Jake Ledoux <contactjakeledoux@gmail.com>
2021-11-30 08:08:44 -0500

    Compile-time sample/input loading

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+#![allow(dead_code)]
+
+const SAMPLE: &str = include_str!("../sample.txt");
+const ADD_SAMPLE: &str = include_str!("../additional_sample.txt");
+const INPUT: &str = include_str!("../input.txt");
+
 fn part_one(input: &str) -> usize {
-    input.lines().count()
+    unimplemented!("{}", input)
 }
 
 fn part_two(input: &str) -> usize {
-    input.lines().count()
+    unimplemented!("{}", input)
 }
 
 fn main() {
-    #[allow(unused_variables)]
-    let [sample, input] = ["sample.txt", "input.txt"]
-        .map(|filename| std::fs::read_to_string(filename).expect("Failed to open file."));
-
-    dbg!(part_one(&sample));
-    dbg!(part_two(&sample));
+    dbg!(part_one(SAMPLE));
+    // dbg!(part_two(SAMPLE));
 }
