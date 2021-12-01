5c7e91b3528d4dd0b849c64d4f888dbd99f6baa0
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-01 09:09:16 -0500

    Reuse input parsing

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,23 @@
 const SAMPLE: &str = include_str!("../sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-fn part_one(input: &str) -> usize {
-    input
-        .split('\n')
+fn part_one(input: &[&str]) -> usize {
+    unimplemented!("{:#?}", input)
+}
+
+fn part_two(input: &[&str]) -> usize {
+    unimplemented!("{:#?}", input)
+}
+
+fn main() {
+    let input: Vec<_> = SAMPLE
+        .lines()
         .filter_map(|s| match s.trim() {
             "" => None,
             _ => Some(s.trim()),
         })
-        .count() // TODO
-}
+        .collect();
 
-fn part_two(input: &str) -> usize {
-    unimplemented!("{}", input)
-}
-
-fn main() {
-    dbg!(part_one(SAMPLE));
-    // dbg!(part_two(SAMPLE));
+    dbg!(part_one(&input));
+    // dbg!(part_two(&input));
 }
