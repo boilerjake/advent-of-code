07bcfe7e36250c4f5011b9bad66224857280d08a
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-01 07:09:35 -0500

    Remove add. sample

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 
 const SAMPLE: &str = include_str!("../sample.txt");
-const ADD_SAMPLE: &str = include_str!("../additional_sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 fn part_one(input: &str) -> usize {
