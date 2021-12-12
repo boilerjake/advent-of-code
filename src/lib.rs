560541fc6325400b03dc836bc50e9144265a68c7
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-12 12:34:50 -0500

    Use tests

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use cached::proc_macro::cached;
+
+pub fn part_one(input: &'static str) -> usize {
+    let input = parse_input(input);
+    0
+}
+
+pub fn part_two(input: &'static str) -> usize {
+    let input = parse_input(input);
+    0
+}
+
+#[cached]
+pub fn parse_input(input: &'static str) -> Vec<&'static str> {
+    input
+        .lines()
+        .filter_map(|s| match s.trim() {
+            "" => None,
+            _ => Some(s.trim()),
+        })
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-#![allow(dead_code)]
+use {{crate_name}}::{part_one, part_two};
 
-const SAMPLE: &str = include_str!("../sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-fn part_one(input: &[&str]) -> usize {
-    unimplemented!("{:#?}", input)
-}
-
-fn part_two(input: &[&str]) -> usize {
-    unimplemented!("{:#?}", input)
-}
-
 fn main() {
-    let input: Vec<_> = SAMPLE
-        .lines()
-        .filter_map(|s| match s.trim() {
-            "" => None,
-            _ => Some(s.trim()),
-        })
-        .collect();
-
-    dbg!(part_one(&input));
-    // dbg!(part_two(&input));
+    println!("Part one: {}", part_one(INPUT));
+    println!("Part two: {}", part_two(INPUT));
 }
