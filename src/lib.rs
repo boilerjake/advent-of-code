3e415cb332eac1cd637e9f08def21a81fda8772b
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-12 14:10:00 -0500

    integration tests -> unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,20 @@ fn parse_input(input: &'static str) -> Vec<&'static str> {
         })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{part_one, part_two};
+
+    const SAMPLE: &str = include_str!("../sample.txt");
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(SAMPLE), 0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(SAMPLE), 0);
+    }
+}
